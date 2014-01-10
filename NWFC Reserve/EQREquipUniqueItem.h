28c84ed976918c71be956e9b8e739e44f87fe520
Dave Hanagan <dhanagan@dhanagan02262.local>
2014-01-09 17:16:56 -0800

    test from nwfc office commit

---
+++ NWFC Reserve/EQREquipUniqueItem.h
@@ -0,0 +1,19 @@
+//
+//  EQREquipUniqueItem.h
+//  NWFC Reserve
+//
+//  Created by Dave Hanagan on 1/9/14.
+//  Copyright (c) 2014 Ham Again LLC. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface EQREquipUniqueItem : NSObject
+
+@property (strong, nonatomic) NSString* key_id;
+@property (strong, nonatomic) NSString* equipTitleItem_foreignKey;
+@property (strong, nonatomic) NSString* name;
+@property (strong, nonatomic) NSString* distinquishing_id;
+@property (strong, nonatomic) NSString* status_level;
+
+@end
---
+++ NWFC Reserve/EQREquipUniqueItem.m
@@ -0,0 +1,13 @@
+//
+//  EQREquipUniqueItem.m
+//  NWFC Reserve
+//
+//  Created by Dave Hanagan on 1/9/14.
+//  Copyright (c) 2014 Ham Again LLC. All rights reserved.
+//
+
+#import "EQREquipUniqueItem.h"
+
+@implementation EQREquipUniqueItem
+
+@end
