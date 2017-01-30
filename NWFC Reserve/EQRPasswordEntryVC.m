8fea93a68eb3fb8a3bbe6031bdcf01f5ca2b316d
Dave Hanagan <billyxshenaniganfilms@hotmail.com>
2017-01-29 21:43:11 -0800

    All EQGetTextElementsWithContext requests are async

--- NWFC Reserve/EQRPasswordEntryVC.m
+++ NWFC Reserve/EQRPasswordEntryVC.m
@@ -11,10 +11,11 @@
 #import "EQRTextElement.h"
 #import "EQRGlobals.h"
 
-@interface EQRPasswordEntryVC ()
+@interface EQRPasswordEntryVC () <EQRWebDataDelegate>
 
 @property (strong, nonatomic) IBOutlet UITextField* passwordField;
 @property (strong, nonatomic) IBOutlet UILabel* incorrectWarning;
+@property (strong, nonatomic) NSMutableArray *passwords;
 
 @end
 
@@ -37,59 +38,86 @@
 -(IBAction)enterButton:(id)sender{
     
     EQRWebData* webData = [EQRWebData sharedInstance];
-    NSMutableArray* tempMuteArray = [NSMutableArray arrayWithCapacity:1];
+    webData.delegateDataFeed = self;
+    
     NSArray* firstArray = @[@"context", @"passwordUser"];
     NSArray* topArray = @[firstArray];
-    [webData queryWithLink:@"EQGetTextElementsWithContext.php" parameters:topArray class:@"EQRTextElement" completion:^(NSMutableArray *muteArray) {
-        
-        for (EQRTextElement* textElementObject in muteArray){
-            [tempMuteArray addObject:textElementObject];
-        }
-    }];
     
-    BOOL passwordSuccess = NO;
-    
-//    NSLog(@"count of passwords from db: %u", [tempMuteArray count]);
-    
-    if ([tempMuteArray count] > 0){
-        
-        for (EQRTextElement* textElement in tempMuteArray){
-            
-//            NSLog(@"this is the password from the db >>%@<<", textElement.text);
-            
-            //check all user passwords
-            if ([self.passwordField.text isEqualToString:textElement.text]){
-                passwordSuccess = YES;
-                break;
-            }
-        }
-    }else{
-        //error handling for no returns
+    if (!self.passwords){
+        self.passwords = [NSMutableArray arrayWithCapacity:1];
     }
+    [self.passwords removeAllObjects];
     
-    //hardcoded password
-    if (EQRAllowHardcodedPassword){
-        
-        if ([EQRHardcodedPassword isEqualToString:self.passwordField.text]){
-            passwordSuccess = YES;
-        }
-    }
+    SEL selector = @selector(addPassword:);
     
+    dispatch_queue_t queue = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0ul);
+    dispatch_async(queue, ^{
+       [webData queryWithAsync:@"EQGetTextElementsWithContext.php" parameters:topArray class:@"EQRTextElement" selector:selector completion:^(BOOL isLoadingFlagUp) {
+          
+           if (self.passwords.count < 1){
+               NSLog(@"EQRPasswordEntryVC > failed to retrieve any password textElements");
+           }
+           
+           BOOL passwordSuccess = NO;
+           
+           for (EQRTextElement* textElement in self.passwords){
+               //check all user passwords
+               if ([self.passwordField.text isEqualToString:textElement.text]){
+                   passwordSuccess = YES;
+                   break;
+               }
+           }
+           
+           //hardcoded password
+           if (EQRAllowHardcodedPassword){
+               if ([EQRHardcodedPassword isEqualToString:self.passwordField.text]){
+                   passwordSuccess = YES;
+               }
+           }
+           
+           if (passwordSuccess){
+               [self.incorrectWarning setHidden:YES];
+               //tell presenting VC if successful or not
+               [self.delegate passwordEntered:passwordSuccess];
+               
+           }else{
+               
+               //inform user of incorrect password and try again
+               [self.incorrectWarning setHidden:NO];
+               self.passwordField.text = @"";
+           }
+           
+       }];
+    });
     
-    if (passwordSuccess){
-        
-        [self.incorrectWarning setHidden:YES];
-        //tell presenting VC if successful or not
-        [self.delegate passwordEntered:passwordSuccess];
-        
-    }else{
-        
-        //inform user of incorrect password and try again
-        [self.incorrectWarning setHidden:NO];
-        self.passwordField.text = @"";
-    }
+//    [webData queryWithLink:@"EQGetTextElementsWithContext.php" parameters:topArray class:@"EQRTextElement" completion:^(NSMutableArray *muteArray) {
+//        
+//        for (EQRTextElement* textElementObject in muteArray){
+//            [tempMuteArray addObject:textElementObject];
+//        }
+//    }];
 }
 
+#pragma mark - EQRWebData delegate methods
+
+-(void)addASyncDataItem:(id)currentThing toSelector:(SEL)action{
+    if (![self respondsToSelector:action]){
+        NSLog(@"EQRPasswordEntryVC > cannot perform selector: %@", NSStringFromSelector(action));
+        return;
+    }
+    
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Warc-performSelector-leaks"
+    [self performSelector:action withObject:currentThing];
+#pragma clang diagnostic pop
+}
+
+-(void)addPassword:(id)currentThing{
+    if (!currentThing){
+        return;
+    }
+    [self.passwords addObject:currentThing];
+}
 
 /*
 #pragma mark - Navigation
